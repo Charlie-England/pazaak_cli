2c53c660a3f19bbe2e5c2aab664044d8f392fd6e
Charlie England <charlie265@live.com>
2020-02-03 17:12:59 -0800

    first commit

---
+++ game.py
@@ -0,0 +1,29 @@
+import random
+from player import Player
+
+def draw_and_update(p1,o1,current_player):
+    card = random.randint(1,10)
+    if current_player:
+        o1.update_field(card)
+        return 1 #return 1 so that next player is player
+    else:
+        p1.update_field(card)
+        return 0 #return 0 so that next player is opponent
+    
+
+def run_game():
+    game_running = True
+    player_name_choice = input("Input player name: ")
+    p1 = Player("standard", player_name_choice)
+    o1 = Player("standard", player_name="opponent 1")
+    current_player = random.randint(0,1) # 0=player 1=opponent
+    while game_running:
+        if p1.score > 20 or o1.score > 20:
+            run_game = False
+        #draw and update
+        current_player = draw_and_update(p1,o1,current_player)
+        #display
+
+
+if __name__ == "__main__":
+    run_game()
---
+++ player.py
@@ -0,0 +1,26 @@
+from resources import *
+import random
+
+class Player():
+    def __init__(self,deck_type="standard",player_name="Player1"):
+        if deck_type == "standard":
+            self.side_deck = standard_deck()
+        self.player_name = player_name
+        self.score = 0
+        self.hand = self.select_hand_from_deck(self.deck)
+        self.player_field = []
+        player.stand = False
+
+    def display_hand():
+        #implement displaying hand and current number
+        pass
+    
+    def select_hand_from_deck(self, deck):
+        hand = []
+        for _ in range(1,5):
+            choice = random.randint(0,len(self.side_deck)-1)
+            hand.append(self.side_deck.pop(choice))
+        return hand
+
+    def update_field(card):
+        self.player_field.append(card)
